c4324c8979870fed6fd0f53f9d3c0fe13289ea3c
shivani chhatre <shivanichhatre@gmail.com>
2022-03-03 14:57:41 +0530

    crud created

--- src/app/pages/demo/demo.module.ts
+++ src/app/pages/demo/demo.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 
 import { DemoRoutingModule } from './demo-routing.module';
 import { ShubhamDemoComponent } from './shubham-demo/shubham-demo.component';
 import { AnkitJDemoComponent } from './ankit-j-demo/ankit-j-demo.component';
+import { ShivaniComponent } from './shivani/shivani.component';
 
 @NgModule({
   declarations: [
     ShubhamDemoComponent,
-    AnkitJDemoComponent
+    AnkitJDemoComponent,
+    ShivaniComponent,
+    
   ],
   imports: [
-    DemoRoutingModule
+    DemoRoutingModule,
+    CommonModule ,
+    FormsModule,
+    ReactiveFormsModule
+
   ]
 })
 export class DemoModule { }
