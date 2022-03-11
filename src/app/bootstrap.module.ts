0cf8663d95fcf6a22201035fd9a18468f6720de6
UTKARSHA NAKHATE <utkarshan123@gmail.com>
2022-03-11 19:24:46 +0530

    some changes are added

--- src/app/bootstrap.module.ts
+++ src/app/bootstrap.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from "@angular/core";
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
 
 @NgModule({
-  exports: [
+  imports: [
+    BsDatepickerModule.forRoot()
+
   ],
+  exports: [
+    BsDatepickerModule
+
+  ]
 })
 export class BootstrapModule {}
