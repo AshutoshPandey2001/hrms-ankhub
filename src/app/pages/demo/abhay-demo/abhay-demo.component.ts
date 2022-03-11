512f36e0a91f38a8c8d5f86299dfd5fac16fffe0
Abhay Mahajan <abhaymahajan0406@gmail.com>
2022-03-11 16:14:22 +0530

    abhay changes

--- src/app/pages/demo/abhay-demo/abhay-demo.component.ts
+++ src/app/pages/demo/abhay-demo/abhay-demo.component.ts
@@ -25,10 +25,11 @@ export class AbhayDemoComponent implements OnInit {
   istableoperation = false;
   selectedIndexd: any;
   selectedObjectd: any;
+  loading = false;
 
-  
+
   constructor(private formBuilder: FormBuilder) {
-    
+
     // this.carForm = this.formBuilder.group({
     //   carmodels: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(9)]],
     //   carcolor: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(13)]]
@@ -40,20 +41,20 @@ export class AbhayDemoComponent implements OnInit {
     // }
 
     this.modForm = this.formBuilder.group({
-      fullname:['',[Validators.required]],
-      email:['',[Validators.email]],
-      mobileno:['',[Validators.minLength(10),Validators.maxLength(10)]],
-      dateob:['',[Validators.required]],
-      gender:['',[Validators.required]],
-      qualifi:['']
+      fullname: ['', [Validators.required]],
+      email: ['', [Validators.email]],
+      mobileno: ['', [Validators.minLength(10), Validators.maxLength(10)]],
+      dateob: ['', [Validators.required]],
+      gender: ['', [Validators.required]],
+      qualifi: ['']
     })
 
     let datatab = localStorage.getItem('MODLIST')
-    if(datatab){
+    if (datatab) {
       this.modlList = JSON.parse(datatab);
     }
 
-   }
+  }
 
   ngOnInit(): void {
   }
@@ -101,7 +102,7 @@ export class AbhayDemoComponent implements OnInit {
 
   // carDelete(id:any) {
   //   this.selectedIndex = this.carsList.findIndex((xt:any)=>xt.id === id);
-    
+
   //   this.carsList.splice(this.selectedIndex, 1);
   //   localStorage.setItem("CARSLIST", JSON.stringify(this.carsList));
   // }
@@ -119,212 +120,230 @@ export class AbhayDemoComponent implements OnInit {
   // };
 
 
-  tabSubmit(from,align){
+  tabSubmit(from, align) {
     this.submittedl = true
     console.log('control works', this.modForm.controls);
-    
-    console.log('value',this.modForm.value);
+
+    console.log('value', this.modForm.value);
     console.log("Controls are valid", this.modlList);
-      if (this.modForm.valid) {
-        this.modForm.value.id = this.tableid();
-        this.modlList.push(this.modForm.value)
-        const type = ['','success'];
+    if (this.modForm.valid) {
+     
+      
+      this.loading = true;
+      setTimeout(() => {
+        
+        this.loading = false;
+        const type = ['', 'success'];
 
-      const color = Math.floor((Math.random() * 1) + 1);
+        const color = Math.floor((Math.random() * 1) + 1);
 
-      $.notify({
+        $.notify({
           icon: "notifications",
           message: "Your form is submitted Successfully!"
 
-      },
-      {
-          type: type[color],
-          timer: 4000,
-          placement: {
+        },
+          {
+            type: type[color],
+            timer: 4000,
+            placement: {
               from: from,
               align: align
-          },
-          template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
-            '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
-            '<i class="material-icons" data-notify="icon">notifications</i> ' +
-            '<span data-notify="title">{1}</span> ' +
-            '<span data-notify="message">{2}</span>' +
-            '<div class="progress" data-notify="progressbar">' +
+            },
+            template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
+              '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
+              '<i class="material-icons" data-notify="icon">notifications</i> ' +
+              '<span data-notify="title">{1}</span> ' +
+              '<span data-notify="message">{2}</span>' +
+              '<div class="progress" data-notify="progressbar">' +
               '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
-            '</div>' +
-            '<a href="{3}" target="{4}" data-notify="url"></a>' +
+              '</div>' +
+              '<a href="{3}" target="{4}" data-notify="url"></a>' +
+              '</div>'
+          });
+         
+      }, 5000);
+      
+      this.modForm.value.id = this.tableid();
+        this.modlList.push(this.modForm.value);
+      this.tabclear()
+    }
+    else {
+      alert("Form is invalid....Please try again!")
+    }
+
+    localStorage.setItem("MODLIST", JSON.stringify(this.modlList));
+
+  }
+
+  tabUpdate(from, align) {
+    
+
+    this.istableoperation = false;
+    this.loading = true;
+      setTimeout(() => {
+        this.loading = false;
+
+    const type = ['', 'warning'];
+
+    const color = Math.floor((Math.random() * 1) + 1);
+
+    $.notify({
+      icon: "notifications",
+      message: "Your Info Updated Successfully!"
+
+    },
+      {
+        type: type[color],
+        timer: 3000,
+        placement: {
+          from: from,
+          align: align
+        },
+        template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
+          '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
+          '<i class="material-icons" data-notify="icon">notifications</i> ' +
+          '<span data-notify="title">{1}</span> ' +
+          '<span data-notify="message">{2}</span>' +
+          '<div class="progress" data-notify="progressbar">' +
+          '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+          '</div>' +
+          '<a href="{3}" target="{4}" data-notify="url"></a>' +
           '</div>'
       });
-        this.tabclear()
-      }
-      else {
-        alert("Form is invalid....Please try again!")
-      }
-      
-      localStorage.setItem("MODLIST", JSON.stringify(this.modlList));
-  
-   }
-  
-   tabUpdate(from,align){
+    }, 5000);
     this.modlList[this.selectedIndexd].fullname = this.modForm.value.fullname;
     this.modlList[this.selectedIndexd].email = this.modForm.value.email;
     this.modlList[this.selectedIndexd].mobileno = this.modForm.value.mobileno;
     this.modlList[this.selectedIndexd].dateob = this.modForm.value.dateob;
     this.modlList[this.selectedIndexd].gender = this.modForm.value.gender;
     this.modlList[this.selectedIndexd].qualifi = this.modForm.value.qualifi;
-  
-      this.istableoperation = false;
-     
-      
-      const type = ['','warning'];
+    this.tabclear();
+  }
 
-      const color = Math.floor((Math.random() * 1) + 1);
+  tabEdit(obj: any, from, align) {
+    this.selectedIndexd = this.modlList.findIndex((xb: any) => xb.id === obj.id);
+    console.log('this.selectedIndexd', this.selectedIndexd)
+    //   this.selectedObject = obj;
 
-      $.notify({
-          icon: "notifications",
-          message: "Your Info Updated Successfully!"
+    this.istableoperation = true;
+    this.modForm.patchValue({
+      fullname: obj.fullname,
+      email: obj.email,
+      mobileno: obj.mobileno,
+      dateob: obj.dateob,
+      gender: obj.gender,
+      qualifi: obj.qualifi
+    })
+    
+    const type = ['', 'info'];
 
-      },
+    const color = Math.floor((Math.random() * 1) + 1);
+
+    $.notify({
+      icon: "notifications",
+      message: "you clicked on edit button!"
+
+    },
       {
-          type: type[color],
-          timer: 3000,
-          placement: {
-              from: from,
-              align: align
-          },
-          template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
-            '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
-            '<i class="material-icons" data-notify="icon">notifications</i> ' +
-            '<span data-notify="title">{1}</span> ' +
-            '<span data-notify="message">{2}</span>' +
-            '<div class="progress" data-notify="progressbar">' +
-              '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
-            '</div>' +
-            '<a href="{3}" target="{4}" data-notify="url"></a>' +
-          '</div>'
-      });
-      this.tabclear();
-   }
-  
-   tabEdit(obj:any,from,align){
-    this.selectedIndexd = this.modlList.findIndex((xb:any)=>xb.id === obj.id);
-       console.log('this.selectedIndexd', this.selectedIndexd)
-     //   this.selectedObject = obj;
-  
-         this.istableoperation = true;
-      this.modForm.patchValue({
-        fullname: obj.fullname,
-        email: obj.email,
-        mobileno:obj.mobileno,
-        dateob:obj.dateob,
-        gender:obj.gender,
-        qualifi:obj.qualifi
-      })
-      const type = ['','info'];
-
-      const color = Math.floor((Math.random() * 1) + 1);
-
-      $.notify({
-          icon: "notifications",
-          message: "you clicked on edit button!"
-
-      },
-      {
-          type: type[color],
-          timer: 3000,
-          placement: {
-              from: from,
-              align: align
-          },
-          template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
-            '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
-            '<i class="material-icons" data-notify="icon">notifications</i> ' +
-            '<span data-notify="title">{1}</span> ' +
-            '<span data-notify="message">{2}</span>' +
-            '<div class="progress" data-notify="progressbar">' +
-              '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
-            '</div>' +
-            '<a href="{3}" target="{4}" data-notify="url"></a>' +
-          '</div>'
-      });
-   }
-  
-   tabDelete(id:any,from,align){
-    this.selectedIndexd = this.modlList.findIndex((xb:any)=>xb.id === id);
-      // let box = prompt("Do you want to delete");
-      if(confirm('Do you want to delet')==true){
-        
-        const type = ['','danger'];
-
-        const color = Math.floor((Math.random() * 1) + 1);
-  
-        $.notify({
-            icon: "notifications",
-            message: "Your Info Deleted Successfully!"
-  
+        type: type[color],
+        timer: 3000,
+        placement: {
+          from: from,
+          align: align
         },
+        template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
+          '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
+          '<i class="material-icons" data-notify="icon">notifications</i> ' +
+          '<span data-notify="title">{1}</span> ' +
+          '<span data-notify="message">{2}</span>' +
+          '<div class="progress" data-notify="progressbar">' +
+          '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+          '</div>' +
+          '<a href="{3}" target="{4}" data-notify="url"></a>' +
+          '</div>'
+      });
+  }
+
+  tabDelete(id: any, from, align) {
+    this.selectedIndexd = this.modlList.findIndex((xb: any) => xb.id === id);
+    // let box = prompt("Do you want to delete");
+    if (confirm('Do you want to delete?') == true) {
+      this.loading = true;
+      setTimeout(() => {
+        this.loading = false;
+      const type = ['', 'danger'];
+
+      const color = Math.floor((Math.random() * 1) + 1);
+
+      $.notify({
+        icon: "notifications",
+        message: "Your Info Deleted Successfully!"
+
+      },
         {
-            type: type[color],
-            timer: 3000,
-            placement: {
-                from: from,
-                align: align
-            },
-            template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
-              '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
-              '<i class="material-icons" data-notify="icon">notifications</i> ' +
-              '<span data-notify="title">{1}</span> ' +
-              '<span data-notify="message">{2}</span>' +
-              '<div class="progress" data-notify="progressbar">' +
-                '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
-              '</div>' +
-              '<a href="{3}" target="{4}" data-notify="url"></a>' +
+          type: type[color],
+          timer: 3000,
+          placement: {
+            from: from,
+            align: align
+          },
+          template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
+            '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
+            '<i class="material-icons" data-notify="icon">notifications</i> ' +
+            '<span data-notify="title">{1}</span> ' +
+            '<span data-notify="message">{2}</span>' +
+            '<div class="progress" data-notify="progressbar">' +
+            '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+            '</div>' +
+            '<a href="{3}" target="{4}" data-notify="url"></a>' +
             '</div>'
         });
         this.modlList.splice(this.selectedIndexd, 1);
-      }else{
-        const type = ['','warning'];
+      }, 2000);
+      
+    } else {
+      const type = ['', 'warning'];
 
-        const color = Math.floor((Math.random() * 1) + 1);
-  
-        $.notify({
-            icon: "notifications",
-            message: "Your Editting cancel!"
-  
-        },
+      const color = Math.floor((Math.random() * 1) + 1);
+
+      $.notify({
+        icon: "notifications",
+        message: "Your Editting cancel!"
+
+      },
         {
-            type: type[color],
-            timer: 3000,
-            placement: {
-                from: from,
-                align: align
-            },
-            template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
-              '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
-              '<i class="material-icons" data-notify="icon">notifications</i> ' +
-              '<span data-notify="title">{1}</span> ' +
-              '<span data-notify="message">{2}</span>' +
-              '<div class="progress" data-notify="progressbar">' +
-                '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
-              '</div>' +
-              '<a href="{3}" target="{4}" data-notify="url"></a>' +
+          type: type[color],
+          timer: 3000,
+          placement: {
+            from: from,
+            align: align
+          },
+          template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
+            '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
+            '<i class="material-icons" data-notify="icon">notifications</i> ' +
+            '<span data-notify="title">{1}</span> ' +
+            '<span data-notify="message">{2}</span>' +
+            '<div class="progress" data-notify="progressbar">' +
+            '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+            '</div>' +
+            '<a href="{3}" target="{4}" data-notify="url"></a>' +
             '</div>'
         });
-      }
-     
-     localStorage.setItem("MODLIST", JSON.stringify(this.modlList));
-   }
-  
-   tabclear(){
+    }
+
+    localStorage.setItem("MODLIST", JSON.stringify(this.modlList));
+  }
+
+  tabclear() {
     this.modForm.reset();
-   }
-  
-   get m(){
+  }
+
+  get m() {
     return this.modForm.controls;
-   }
-  
-   tableid() {
-      return '_' + Math.random().toString(36).substr(2, 9);
-    };
-    
+  }
+
+  tableid() {
+    return '_' + Math.random().toString(36).substr(2, 9);
+  };
+
 }
