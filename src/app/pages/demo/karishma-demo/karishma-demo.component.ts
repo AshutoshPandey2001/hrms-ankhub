c346e77f1b8eb82008dc6c044215de6830b28da2
Karishma Shende <karishmashende321@gmail.com>
2022-03-12 18:44:50 +0530

    crud

--- src/app/pages/demo/karishma-demo/karishma-demo.component.ts
+++ src/app/pages/demo/karishma-demo/karishma-demo.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-karishma-demo',
@@ -17,11 +21,11 @@ export class KarishmaDemoComponent implements OnInit {
 
   
 
-  constructor(private formBuilder:FormBuilder) {
+  constructor(private formBuilder:FormBuilder,private toastr: ToastrService,private ngxService: NgxUiLoaderService) {
      this.employeeForm = this.formBuilder.group ({
       name:['', [Validators.required]],
-      email:['' , [Validators.required]],
-      mobile:['', [Validators.required,Validators.maxLength(10),]],
+      email:['' , [Validators.required,Validators.email]],
+      mobile:['', [Validators.required,Validators.maxLength(11),]],
       dob:['',[Validators.required]],
       gender:['',[Validators.required]],
       qual:['',[Validators.required]]
@@ -39,22 +43,35 @@ export class KarishmaDemoComponent implements OnInit {
 
   Save(){
     this.submitted=true;
-
+    {
+      this.ngxService.start(); 
+      setTimeout(() => {
+        this.ngxService.stop(); 
+      }, 2000);
+    }
     if(this.employeeForm.valid){
       this.employeeList.push(this.employeeForm.value)
       console.log("Submit Sucessfully",this.employeeForm.value);
-      alert("Form is valid.... submitted Sucessfully...!")
+      this.toastr.success('Form is valid', ' Submitted sucessfully...!');
       let ref = document.getElementById('cancel')
       ref?.click();
+     
    }
    else{
-    alert("Form is not-valid.... please try again...!")
+    this.toastr.error('Form is not valid', 'Please try again...!');
    }
     this.employeeForm.reset();
+
     localStorage.setItem("EMPLOYEE_LIST", JSON.stringify(this.employeeList))
   }
 
   Update(){
+    {
+      this.ngxService.start(); 
+      setTimeout(() => {
+        this.ngxService.stop(); 
+      }, 2000);
+    }
     this.editoperation=false;
     let ref = document.getElementById('cancel')
     ref?.click();
@@ -64,7 +81,9 @@ export class KarishmaDemoComponent implements OnInit {
     this.employeeList[this.selectedindex].dob=this.employeeForm.value.dob;
     this.employeeList[this.selectedindex].gender=this.employeeForm.value.gender;
     this.employeeList[this.selectedindex].qual=this.employeeForm.value.qual;
+    this.toastr.success('Form is valid', 'Details Update sucessfully...!');
     this.clear();
+   
   
     localStorage.setItem("EMPLOYEE_LIST", JSON.stringify(this.employeeList))
   }
@@ -87,8 +106,30 @@ export class KarishmaDemoComponent implements OnInit {
 
   delete(index:any){
     console.log("Delete",index);
-    this.employeeList.splice(index,1)
+    this.employeeList.splice(index,1);
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't to delete this file..!",
+      icon: 'warning',
+      allowEscapeKey: false,
+      allowOutsideClick: false,
+      confirmButtonColor: '#3085d6',
+      confirmButtonText: 'Yes, delete it!',
+      showCancelButton: true,
+      cancelButtonColor: '#d33',
+      cancelButtonText:'Cancle'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        )
+      }
+    })
+    localStorage.setItem("EMPLOYEE_LIST", JSON.stringify(this.employeeList))
   }
+
   get f(){
     return this.employeeForm.controls
   }
