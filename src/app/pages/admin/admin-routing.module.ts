7aa5067ab38eb22e53d8a94b2947c519b593348d
Ashutosh Pandey <ap1495813@gmail.com>
2022-03-17 14:08:55 -0700

    ashutosh changes this

--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { CoursesComponent } from './courses/courses.component';
+import { QualificationComponent } from './qualification/qualification.component';
 
 const routes: Routes = [
   {
@@ -15,6 +16,9 @@ const routes: Routes = [
       },
       {
         path: 'courses', component: CoursesComponent
+      },
+      {
+        path: 'qualification', component: QualificationComponent
       }
     ]
   }
--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AllModule } from 'app/allModule.module';
 import { CoursesComponent } from './courses/courses.component';
+import { QualificationComponent } from './qualification/qualification.component';
 
 
 @NgModule({
   declarations: [
-    CoursesComponent
+    CoursesComponent,
+    QualificationComponent
   ],
   imports: [
     AllModule,
