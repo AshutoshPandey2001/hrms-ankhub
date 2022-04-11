26b41262446787dc22601d83a07b52cc6faf9a11
Abhijeet Dudhe <abhidudhe2000@gmail.com>
2022-04-11 18:38:15 +0530

    new changes

--- src/app/pages/admin/position/position.component.ts
+++ src/app/pages/admin/position/position.component.ts
@@ -10,6 +10,12 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./position.component.scss']
 })
 export class PositionComponent implements OnInit {
+  employeeForm:FormGroup;
+  employeeList:any=[''];
+  subBtn=false;
+  editbtn=false;
+  selectobj:any;
+  searchitem='';
   modalRef: BsModalRef;
   config: ModalOptions = {
     backdrop: true, //backgroud color as dark
@@ -17,138 +23,282 @@ export class PositionComponent implements OnInit {
     ignoreBackdropClick: true, //Close on outside click
     animated: true, //Add animation on open close
   };
-  isUpdate = false;
-employeeList : any = [];
-  selectIndex:any;
-  searchKey = '';
-  employeeForm : FormGroup;
-  dataSub = false;
-  loaDing = false;
-  types=['Fresher','Junior Software Engineer','Senior Software Engineer'] 
+//   isUpdate = false;
+// employeeList : any = [];
+//   selectIndex:any;
+//   searchKey = '';
+//   employeeForm : FormGroup;
+//   dataSub = false;
+//   loaDing = false;
+//   types=['Fresher','Junior Software Engineer','Senior Software Engineer'] 
 
-  constructor(private FormBuilder: FormBuilder,private modalService: BsModalService,private ngxService: NgxUiLoaderService,private toastr: ToastrService) {
-    this.employeeForm = FormBuilder.group({
-      employeePosition : ['',[Validators .required]],
-      type:['',[Validators .required]]
+   constructor(private FormBuilder: FormBuilder,private modalService: BsModalService,private ngxService: NgxUiLoaderService,private toastr: ToastrService) {
+    this.employeeForm=this.FormBuilder.group({
+      employeePosition:['',[Validators.required]],
+      type:['',[Validators.required]]
     })
-    let data1 = localStorage.getItem('EMPLOYEE');
-    if(data1){
-      this.employeeList = JSON.parse(data1);
+    let data= localStorage.getItem("Employee_LIST");
+    if(data){
+      this.employeeList=JSON.parse(data);
     }
-
-   }
-
-  ngOnInit(): void {
   }
- openModal(template: TemplateRef<any>) {
-this.modalRef = this.modalService.show(template, this.config);
+//     this.employeeForm = FormBuilder.group({
+//       employeePosition : ['',[Validators .required]],
+//       type:['',[Validators .required]]
+//     })
+//     let data1 = localStorage.getItem('EMPLOYEE');
+//     if(data1){
+//       this.employeeList = JSON.parse(data1);
+//     }
+
+//    }
+
+ ngOnInit(): void {
+  }
+  openModal(template: TemplateRef<any>) {
+    this.subBtn=false;
+    this.clear();
+    this.modalRef = this.modalService.show(template, this.config);
+
   }
 
   closeModal() {
     this.modalRef.hide();
+    this.editbtn=false;
+    
   }
 
-  Submit(){
-  this.dataSub = true;
 
-  if (this.employeeForm.valid){
- this.closeModal();
-   
-    this.ngxService.start();
-
-    setTimeout(() => {  
-      this.ngxService.stop();
-      this.employeeForm.value.id = this.randomID();
-      this.employeeList.push(this.employeeForm.value)
-      localStorage.setItem("EMPLOYEE", JSON.stringify(this.employeeList));
-      console.log('table', this.employeeList);
-     
-      this.employeeClear()
-      this.toastr.success("Thank you !", "Your Data is Submitted Sucessfully..!");
-    }, 2000);
-   
-}
-else {
-  this.toastr.error("Please try again..!", "Invalid  Data !");
- }
-
-}
-
-Edit(obj:any , template: TemplateRef<any>){
-    this.modalRef = this.modalService.show(template, this.config);
-  this.selectIndex= this.employeeList.findIndex((x : any )=> x.id === obj.id);
-  this.employeeForm.patchValue({
-    employeePosition : obj.employeePosition ,
-    type: obj.type
-  })
-
-      this.isUpdate = true;
-}
-
-Update(){
- 
-  this.closeModal();
-    this.ngxService.start();
-    setTimeout(() => { 
-      this.employeeList[this.selectIndex].employeePosition = this.employeeForm.value.employeePosition;
-      this.employeeList[this.selectIndex].type = this.employeeForm.value.type;
-      localStorage.setItem("EMPLOYEE", JSON.stringify(this.employeeList));
-      this.employeeClear()
-      this.ngxService.stop();
-      this.isUpdate = false ;
+  Submit() {
+    this.subBtn=true;
+    if(this.employeeForm.valid){
     
-      this.toastr.success("Thank you !", "Your Data is Updated Sucessfully..!");
-      
-    }, 2000);
-}
+     this.closeModal();
+      this.ngxService.start(); 
+      setTimeout(() => {
+        this.employeeForm.value.id=this.randomID();
+    this.employeeList.push(this.employeeForm.value);
+    localStorage.setItem("Employee_LIST" , JSON.stringify(this.employeeList));
+      this.clear();
+      this.toastr.success('Submitted Successfully!', 'Details Valid!');
 
-Delete(id:any){
-
-  Swal.fire({
-    title: 'Are you sure?',
-    text: "You won't be able to revert this!",
-    icon: 'warning',
-    allowEscapeKey: false,
-    allowOutsideClick: false,
-    confirmButtonColor: '#3085d6',
-    confirmButtonText: 'Yes, delete it!',
-    showCancelButton: true,
-    cancelButtonColor: '#d33',
-    cancelButtonText:'Cancle'
-  }).then((result) => {
-    if (result.isConfirmed) {
-      this.ngxService.start();
-      setTimeout(() => {  
- 
-        setTimeout(() => {  
-        Swal.fire("Deleted!", "Your file has been deleted.", "success");
-          this.toastr.success(" Deleted Sucessfully..!");
-    }, 1000);
-   
-      this.ngxService.stop();
-      this.selectIndex= this.employeeList.findIndex((x : any )=> x.id === id);    
-      this.employeeList.splice(this.selectIndex,1);
-      localStorage.setItem("EMPLOYEE", JSON.stringify(this.employeeList));
+        this.ngxService.stop(); 
       }, 2000);
+     
+    }else {
+       this.toastr.error("Please try again..!", "Invalid  Data !");
+        }
+      
+  }
+
+
+  Update(){
+    this.subBtn=true;
+    if(this.employeeForm.valid){
+    this.editbtn=false;
+    this.closeModal();
+   this.ngxService.start(); 
+   setTimeout(() => {
+    this.employeeList[this.selectobj].employeePosition=this.employeeForm.value.employeePosition; 
+   this.employeeList[this.selectobj].type=this.employeeForm.value.type; 
+
+    localStorage.setItem("Employee_LIST" , JSON.stringify(this.employeeList));
+    this.toastr.success('Updated Successfully!', 'Details Valid!');
     
-    }
-  }) 
+    this.clear();
+     this.ngxService.stop(); 
+   }, 2000);
+  }else {
+    this.toastr.error("Please try again..!", "Invalid  Data !");
+     }
+   
+  
+  }
+
+
+  Edit(obj:any , template: TemplateRef<any>){
+    this.editbtn=true;   
+          this.modalRef = this.modalService.show(template, this.config);      
+    this.selectobj=this.employeeList.findIndex((x: any) => x.id === obj.id);
+      this.employeeForm.patchValue({
+        employeePosition:obj.employeePosition,
+        type:obj.type
+      })
+  }
+
+
+  Delete(id:any){
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      allowEscapeKey: false,
+      allowOutsideClick: false,
+      confirmButtonColor: '#3085d6',
+      confirmButtonText: 'Yes, delete it!',
+      showCancelButton: true,
+      cancelButtonColor: '#d33',
+      cancelButtonText:'Cancle'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.ngxService.start(); 
+        setTimeout(() => {
+          this.selectobj=this.employeeList.findIndex((x: any) => x.id === id);
+          this.employeeList.splice(this.selectobj,1);
+      localStorage.setItem("Employee_LIST" , JSON.stringify(this.employeeList));
+      Swal.fire(
+          
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+        
+      )
+      this.toastr.success('Successfully!', 'Entry Deleted !');
+
+          this.ngxService.stop(); 
+        }, 2000);
+       
+        
+      }
+    })
+    
+
+  }
+ randomID() {
+  return '_' + Math.random().toString(36).substr(2, 9);
 }
 
-employeeClear(){
-this.employeeForm.reset();
+  get f(){
+    return this.employeeForm.controls;
+  }
+
+  clear(){
+    this.employeeForm.reset();
+  }
 }
 
 
-randomID()
-{
-return '_' + Math.random().toString(36).substr(2, 9);
-}
+//   url="./assets/img/img_avatar.png"
+//   onselectFile(a){
+//     if(a.target.files)
+//   {
+//     var reader = new FileReader();
+//     reader.readAsDataURL(a.target.files[0])
+//     reader.onload=(event:any)=>{
+//       this.url=event.target.result;
+//     }
+//   }
+//   }
+//  openModal(template: TemplateRef<any>) {
+// this.modalRef = this.modalService.show(template, this.config);
+//   }
+
+//   closeModal() {
+//     this.modalRef.hide();
+  
+//   }
+
+//   Submit(){
+//   this.dataSub = true;
+
+//   if (this.employeeForm.valid){
+//  this.closeModal();
+   
+//     this.ngxService.start();
+
+//     setTimeout(() => {  
+//       this.ngxService.stop();
+//       this.employeeForm.value.id = this.randomID();
+//       this.employeeList.push(this.employeeForm.value)
+//       localStorage.setItem("EMPLOYEE", JSON.stringify(this.employeeList));
+//       console.log('table', this.employeeList);
+     
+//       this.employeeClear()
+//       this.toastr.success("Thank you !", "Your Data is Submitted Sucessfully..!");
+//     }, 2000);
+   
+// }
+// else {
+//   this.toastr.error("Please try again..!", "Invalid  Data !");
+//  }
+
+// }
+
+// Edit(obj:any , template: TemplateRef<any>){
+//     this.modalRef = this.modalService.show(template, this.config);
+//   this.selectIndex= this.employeeList.findIndex((x : any )=> x.id === obj.id);
+//   this.employeeForm.patchValue({
+//     employeePosition : obj.employeePosition ,
+//     type: obj.type
+//   })
+
+//       this.isUpdate = true;
+// }
+
+// Update(){
+ 
+//   this.closeModal();
+//     this.ngxService.start();
+//     setTimeout(() => { 
+//       this.employeeList[this.selectIndex].employeePosition = this.employeeForm.value.employeePosition;
+//       this.employeeList[this.selectIndex].type = this.employeeForm.value.type;
+//       localStorage.setItem("EMPLOYEE", JSON.stringify(this.employeeList));
+//       this.employeeClear()
+//       this.ngxService.stop();
+//       this.isUpdate = false ;
+    
+//       this.toastr.success("Thank you !", "Your Data is Updated Sucessfully..!");
+      
+//     }, 2000);
+// }
+
+// Delete(id:any){
+
+//   Swal.fire({
+//     title: 'Are you sure?',
+//     text: "You won't be able to revert this!",
+//     icon: 'warning',
+//     allowEscapeKey: false,
+//     allowOutsideClick: false,
+//     confirmButtonColor: '#3085d6',
+//     confirmButtonText: 'Yes, delete it!',
+//     showCancelButton: true,
+//     cancelButtonColor: '#d33',
+//     cancelButtonText:'Cancle'
+//   }).then((result) => {
+//     if (result.isConfirmed) {
+//       this.ngxService.start();
+//       setTimeout(() => {  
+ 
+//         setTimeout(() => {  
+//         Swal.fire("Deleted!", "Your file has been deleted.", "success");
+//           this.toastr.success(" Deleted Sucessfully..!");
+//     }, 1000);
+   
+//       this.ngxService.stop();
+//       this.selectIndex= this.employeeList.findIndex((x : any )=> x.id === id);    
+//       this.employeeList.splice(this.selectIndex,1);
+//       localStorage.setItem("EMPLOYEE", JSON.stringify(this.employeeList));
+//       }, 2000);
+    
+//     }
+//   }) 
+// }
+
+// employeeClear(){
+// this.employeeForm.reset();
+// }
 
 
-get t(){
-  return this.employeeForm.controls
-}
+// randomID()
+// {
+// return '_' + Math.random().toString(36).substr(2, 9);
+// }
 
 
-}
+// get t(){
+//   return this.employeeForm.controls
+// }
+
+
+// }
