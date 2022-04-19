0b47c9d5bb335853b5887255e372e882932914b7
DESKTOP-15M7SGM\HP <shubhuramtekkar@gmail.com>
2022-04-19 21:36:47 +0530

    fixed css changes

--- src/app/pages/sdk/final-structure/final-structure.component.ts
+++ src/app/pages/sdk/final-structure/final-structure.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-final-structure',
@@ -6,27 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./final-structure.component.scss']
 })
 export class FinalStructureComponent implements OnInit {
-  settings = {
-    columns: {
-      id: {
-        title: 'ID'
-      },
-      name: {
-        title: 'Full Name'
-      },
-      username: {
-        title: 'User Name'
-      },
-      email: {
-        title: 'Email'
-      }
-    }
+  modalRef: BsModalRef;
+  config: ModalOptions = {
+    backdrop: true, //backgroud color as dark
+    keyboard: false, //Close on Esc button
+    ignoreBackdropClick: true, //Close on outside click
+    animated: true, //Add animation on open close
   };
-  constructor() { }
+
+  constructor(private modalService: BsModalService) { }
 
   ngOnInit(): void {
   }
 
-  
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, this.config);
+  }
+
+  closeModal() {
+    this.modalRef.hide();
+  }
+
+  submitData() {
+    console.log("You Clicked on Submit button");
+  }
 
 }
